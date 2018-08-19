7d2de9301bebe70a4f84111bdf89479a0ebb9786
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-08-20 03:17:45 +0900

    images transfer to gcs

---
+++ lit/config/initializers/paperclip.rb
@@ -0,0 +1,29 @@
+if Rails.env.production?
+  Paperclip::Attachment.default_options.merge!(
+    storage: :fog,
+    fog_credentials: {
+      provider: 'Google',
+      google_project: ENV.fetch('GOOGLE_PROJECT'),
+      google_json_key_location: ENV.fetch('GOOGLE_CREDENTIALS')
+    },
+    fog_directory: 'lit-storage-production'
+  )
+
+  Spree::Image.attachment_definitions[:attachment].delete(:url)
+  Spree::Image.attachment_definitions[:attachment].delete(:path)
+end
+
+if Rails.env.development? && ENV['GOOGLE_PROJECT'] && ENV.fetch('GOOGLE_CREDENTIALS')
+  Paperclip::Attachment.default_options.merge!(
+    storage: :fog,
+    fog_credentials: {
+      provider: 'Google',
+      google_project: ENV.fetch('GOOGLE_PROJECT'),
+      google_json_key_location: ENV.fetch('GOOGLE_CREDENTIALS')
+    },
+    fog_directory: 'lit-storage-development'
+  )
+
+  Spree::Image.attachment_definitions[:attachment].delete(:url)
+  Spree::Image.attachment_definitions[:attachment].delete(:path)
+end
