9e3b59dd3ddd38706a5938e2ed4ef140aa866a71
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-10-29 00:00:08 +0900

    changed logo

--- lit/config/initializers/spree.rb
+++ lit/config/initializers/spree.rb
@@ -30,7 +30,7 @@ Spree.config do |config|
   # Frontend:
 
   # Custom logo for the frontend
-  # config.logo = "logo/solidus_logo.png"
+  config.logo = "loveistokyo/android-chrome-192x192.png"
 
   # Template to use when rendering layout
   # config.layout = "spree/layouts/spree_application"
@@ -39,7 +39,7 @@ Spree.config do |config|
   # Admin:
 
   # Custom logo for the admin
-  # config.admin_interface_logo = "logo/solidus_logo.png"
+  config.admin_interface_logo = "loveistokyo/android-chrome-192x192.png"
 
   # Gateway credentials can be configured statically here and referenced from
   # the admin. They can also be fully configured from the admin.
