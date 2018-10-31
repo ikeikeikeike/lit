59b03a38f6db7f1d0fe9794ec2073e4b60c910ab
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-11-01 01:53:02 +0900

    use slider at top instead

--- lit/config/initializers/spree.rb
+++ lit/config/initializers/spree.rb
@@ -30,7 +30,7 @@ Spree.config do |config|
   # Frontend:
 
   # Custom logo for the frontend
-  config.logo = "loveistokyo/android-chrome-192x192.png"
+  config.logo = "loveistokyo/loveistokyo.png"
 
   # Template to use when rendering layout
   # config.layout = "spree/layouts/spree_application"
@@ -39,7 +39,7 @@ Spree.config do |config|
   # Admin:
 
   # Custom logo for the admin
-  config.admin_interface_logo = "loveistokyo/android-chrome-192x192.png"
+  config.admin_interface_logo = "loveistokyo/72width.png"
 
   # Gateway credentials can be configured statically here and referenced from
   # the admin. They can also be fully configured from the admin.
