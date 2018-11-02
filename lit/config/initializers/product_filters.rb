5dc0b61c09d2862bbce4eaa6ba33a6e4b4203be9
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-11-03 05:06:32 +0900

    product filters

--- lit/config/initializers/product_filters.rb
+++ lit/config/initializers/product_filters.rb
@@ -1,17 +1,17 @@
 Rails.application.configure do
   config.after_initialize do
-
-    Spree::Property.all.each do |prop|
-      Spree::Product.add_search_scope :"#{prop.name}_anything" do |*opts|
-        conds = opts.map { |o| Spree::Core::ProductFilters.brand_filter[:conds][o] }.reject(&:nil?)
-        scope = conds.shift
-        conds.each do |new_scope|
-          scope = scope.or(new_scope)
+    if ActiveRecord::Base.connection.active?
+      Spree::Property.all.each do |prop|
+        Spree::Product.add_search_scope :"#{prop.name}_anything" do |*opts|
+          conds = opts.map { |o| Spree::Core::ProductFilters.brand_filter[:conds][o] }.reject(&:nil?)
+          scope = conds.shift
+          conds.each do |new_scope|
+            scope = scope.or(new_scope)
+          end
+          Spree::Product.with_property(prop.name).where(scope)
         end
-        Spree::Product.with_property(prop.name).where(scope)
       end
     end
-
   end
 end
 
